bfaeb5eb971bc40995db21dd789ffbf2e5980eec
Priyanshu Bhattacharya <73550440+Priyanshu1080@users.noreply.github.com>
2024-06-30 22:47:23 +0530

    Update Indian News.py

--- Indian News.py
+++ Indian News.py
@@ -5,95 +5,75 @@
 
 
 import pandas as pd
+import requests
 
 
 # In[2]:
 
 
-import requests
-
-
-# In[15]:
-
-
 api_key="fdd3aefe-34bb-461e-b630-146be8f99bc6"
 
 
-# In[34]:
+# In[3]:
 
 
 api_url = "https://content.guardianapis.com/search"
 page_link = "https://www.theguardian.com/india"
 
 
-# In[35]:
+# In[4]:
 
 
 article_id = page_link.split('/')[-1]
 
-
-# In[49]:
-
-
-params = {
-    "q": article_id,  
-    "api-key": api_key,     
-    "page": current_page_number
-}
-
-
-# In[50]:
-
-
-response = requests.get(api_url, params=params)
-print(response)
-
-
-# In[51]:
-
-
-request = requests.Request('GET', api_url, params=params)
-
-
-# In[52]:
-
-
-data = response.json()
-print(data)
-
-
-# In[53]:
-
-
 print(article_id)
 
 
-# In[54]:
+# In[5]:
 
 
-current_page_number = 1
+total_pages = 70
 
 
-# In[55]:
+# In[6]:
 
 
-generated_url = request.prepare().url
+ans=[]
 
 
-# In[56]:
+# In[7]:
 
 
-print(generated_url)
+for current_page_number in range(1, total_pages + 1):
+    params = {
+        "q": article_id,
+        "api-key": api_key,
+        "page": current_page_number
+    }
+    
+    response = requests.get(api_url, params=params)
+    
+    if response.status_code == 200:
+        data = response.json()
+        ans.append(data)
+    else:
+        print(f"Failed to retrieve page {current_page_number}")
+        break  # Break the loop if there's an issue with the request
 
 
-# In[72]:
+# In[8]:
+
+
+ans
+
+
+# In[9]:
 
 
 urlList=[]
-    
 
 
-# In[73]:
+# In[10]:
 
 
 for i in range(1,7254):
@@ -103,111 +83,61 @@ for i in range(1,7254):
     urlList.append(c)
 
 
-# In[74]:
+# In[11]:
 
 
 urlList
 
 
-# In[83]:
+# In[12]:
 
 
-fourUrls=[]
+len(ans)
 
 
-# In[86]:
+# In[13]:
 
 
-info=[]
+ans[0]['response']['results'][1]['webTitle']
 
 
-# In[84]:
+# In[14]:
 
 
-for i in range(1,5):
-    a = "https://content.guardianapis.com/search?q=india&api-key=fdd3aefe-34bb-461e-b630-146be8f99bc6&page="
-    b= str(i)
-    c=a+b
-    fourUrls.append(c)
+finalNews=[]
 
 
-# In[94]:
+# In[15]:
 
 
-fourUrls
-
-
-# In[99]:
-
-
-def json(url):
-    response=requests.get(url).json()
-    info.append(response)
-    
-    
-
-
-# In[100]:
-
-
-info=[]
-
-
-# In[101]:
-
-
-output = [json(url) for url in fourUrls]
-
-
-# In[102]:
-
-
-info
-
-
-# In[91]:
-
-
-len(info)
-
-
-# In[111]:
-
-
-finalList=[]
-
-
-# In[112]:
-
-
-for i in range(0,4):
+for i in range(0,total_pages):
     for j in range(0,10):
-        values=dict(HeadLines=info[i]['response']['results'][j]['webTitle'],
-        SectionName=info[i]['response']['results'][j]['sectionName'],
-                   PublishDate=info[i]['response']['results'][j]['webPublicationDate'])
-        finalList.append(values)
+        value = dict(
+            webTitle = ans[i]['response']['results'][j]['webTitle'],
+        sectionName = ans[i]['response']['results'][j]['sectionName'],
+        publicationDate = ans[i]['response']['results'][j]['webPublicationDate']),
+        finalNews.append(value)
 
 
-# In[113]:
+# In[16]:
 
 
-finalList
+finalNews
 
 
-# In[114]:
+# In[17]:
 
 
-new_data = pd.DataFrame(finalList)
+datanew = pd.DataFrame(finalNews)
 
 
-# In[115]:
+# In[18]:
 
 
-new_data
+datanew
 
 
 # In[ ]:
-
 
 
 
